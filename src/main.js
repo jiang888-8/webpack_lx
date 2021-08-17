8bf43e1228e67a98998023e7ec4513dffc46cfdf
jiangdou <1565133920@qq.com>
2021-08-17 19:58:21 +0800

    自动生成HTML文件

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import $ from "jquery";
+
+$(function () {
+  $("#ys li:nth-child(odd)").css("color", "#04f4bc");
+  $("#ys li:nth-child(even)").css("color", "#0f8b4c");
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
-import path from "path";
+const path = require("path");
+// 引入自动生成 html 的插件
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/main.js",
@@ -6,4 +8,9 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html", // 以此为基准生成打包后html文件
+    }),
+  ],
 };
